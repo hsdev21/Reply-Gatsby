fc48a83c3423fcc0cf676e574207e2d155f2c90b
hsdev21 <88560663+hsdev21@users.noreply.github.com>
2021-10-27 17:03:19 -0700

    Updated mobile hero image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -615,7 +615,7 @@ export const query = graphql`
         }
       }
     }
-    mobileHero: file(relativePath: { eq: "hero-mobile.jpg" }) {
+    mobileHero: file(relativePath: { eq: "hero-mobile-new.jpg" }) {
       childImageSharp {
         fluid(quality: 100) {
           ...GatsbyImageSharpFluid_withWebp_noBase64
